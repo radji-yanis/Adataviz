96e74fbb4396ee108a6ec87ade8d92800332fd8f
radji-yanis <radjiyanis@gmail.com>
2026-07-08 14:41:29 +0200

    mise a jour CSS/HTML

--- src/counter.js
+++ src/counter.js
@@ -1,2 +1,8 @@
+fetch("https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/ilots-de-fraicheur-espaces-verts-frais/records?limit=20")
+.then((data)=> data.json())
+.then((response)=> console.log(response)).catch((error) => console.error (error))
 
 
+
+    
+ 
